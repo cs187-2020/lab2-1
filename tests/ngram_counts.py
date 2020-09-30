7ae0bdfb5f946196e07f0e06d847812ecb355ff2
cs187staff <dengyuntian@g.harvard.edu>
2020-09-30 19:28:15 +0000

    automatic update

--- tests/ngram_counts.py
+++ tests/ngram_counts.py
@@ -1,6 +1,6 @@
 test = {   'name': 'ngram_counts',
     'points': 1,
-    'suites': [   {   'cases': [{'code': '>>> sum(len(unigram_counts[cntxt].keys()) for cntxt in unigram_counts.keys())\n58', 'hidden': False, 'locked': False}],
+    'suites': [   {   'cases': [{'code': '>>> sum(len(unigram_counts[cntxt]) for cntxt in unigram_counts)\n58', 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
